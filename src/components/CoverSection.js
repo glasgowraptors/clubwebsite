85e6546a255cf89aeca91cc2935c8e0a4c3bccef
sheldon <sheldonfish88@gmail.com>
2021-06-16 15:32:56 +0100

    add video webkit-playsinline for safari iOS

--- src/components/CoverSection.js
+++ src/components/CoverSection.js
@@ -7,7 +7,7 @@ import covervideo from '../assets/videos/vid-01.mp4';
 function CoverSection() {
     return (
         <div className='cover-container'>
-            <video src={covervideo} autoPlay loop muted playsInline /> 
+            <video src={covervideo} autoPlay loop muted playsinline webkit-playsinline /> 
             <h1>Welcome to</h1>
             <h1>Glasgow Raptors Rugby Club</h1>
             <h3> LGBTQ+ Inclusive </h3>
